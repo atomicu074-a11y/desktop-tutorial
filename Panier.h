9f2e2356e872a822bad657229d63d09cbf1f321e
atomicu074-a11y <atomicu074@gmail.com>
2026-05-04 23:48:56 +0200

    Update and rename Panier.cpp to Panier.h

---
+++ Panier.h
@@ -0,0 +1,17 @@
+#ifndef PANIER_H
+#define PANIER_H
+
+#include "Produit.h"
+#include <vector>
+
+class Panier {
+private:
+    vector<Produit*> articles;
+public:
+    void ajouterProduit(Produit* p);
+    double calculerMontantTotal() const;
+    int getNbArticles() const { return articles.size(); }
+    void afficherFacture() const;
+};
+
+#endif
