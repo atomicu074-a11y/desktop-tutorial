4a5927ed5fca6315dcb12c135834b3fd627acc89
atomicu074-a11y <atomicu074@gmail.com>
2026-05-05 15:22:40 +0200

    Add product management and invoice display to Panier

--- Panier.cpp
+++ Panier.cpp
@@ -1,5 +1,7 @@
 #include "Panier.h"
+#include <iostream>
 
+using namespace std;
 void Panier::ajouterProduit(Produit* p) {
     if (p->getStock() > 0) {
         articles.push_back(p);
@@ -8,4 +10,26 @@ void Panier::ajouterProduit(Produit* p) {
     } else {
         cout << "-> Erreur: Stock épuisé !" << endl;
     }
+}
+double Panier::calculerMontantTotal() const {
+    double total = 0;
+    for (Produit* p : articles) {
+        total += p->calculerPrixFinal();
+    }
+    if (total > 500) total *= 0.95; 
+    return total;
+}
+int Panier::getNbArticles() const { 
+    return articles.size(); 
+}
+void Panier::afficherFacture() const {
+    cout << "\n--- VOTRE FACTURE ---" << endl;
+    if (articles.empty()) {
+        cout << "Panier vide." << endl;
+        return;
+    }
+    for (Produit* p : articles) {
+        cout << "- " << p->getNom() << " : " << p->calculerPrixFinal() << " DH" << endl;
+    }
+    cout << "TOTAL (TTC + Remises): " << calculerMontantTotal() << " DH" << endl;
 }
