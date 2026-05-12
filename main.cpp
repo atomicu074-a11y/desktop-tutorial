221557ecf9b3f090eae9efe7353a5359a5537fa5
laylazmehrir6-dotcom <laylazmehrir6@gmail.com>
2026-05-12 23:28:47 +0100

    add comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "Interface.h"
 
+// Point d'entrée du programme
 int main() {
-    Ui boutique;
+    Ui boutique;     // Création de l'interface utilisateur
     boutique.lancer(); // Lance la boucle FTXUI
-    return 0;
+    return 0;    // Fin normale du programme
 }
 
 
