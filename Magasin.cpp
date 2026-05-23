dc74287e02f9754ad11fd7418833c6304612079a
doaa-Br <atomicu074@gmail.com>
2026-05-23 23:38:23 +0200

    Replace Produit with ProduitElectronique in Magasin

--- Magasin.cpp
+++ Magasin.cpp
@@ -2,26 +2,23 @@
 #include <fstream>
 #include <iomanip>
 
-// constructeur Produit
-Produit::Produit(int id, std::string nom, std::string cat, double p, int s) 
-    : id_(id), nom_(nom), categorie_(cat), prix_(p), stock_(s) {}
-
 // initialisation du magasin avec les produits
 Magasin::Magasin() {
-    produits_.push_back(Produit(1, "ordinateur portable", "informatique", 899.99, 5));
-    produits_.push_back(Produit(2, "souris sans fil", "accessoire", 29.99, 20));
-    produits_.push_back(Produit(3, "clavier mecanique", "accessoire", 149.99, 8));
-    produits_.push_back(Produit(4, "casque audio", "audio", 199.99, 12));
-    produits_.push_back(Produit(5, "ecran 27 pouces", "informatique", 299.99, 3));
-    produits_.push_back(Produit(6, "hub usb-c", "connectique", 49.99, 15));
-       produits_.push_back(Produit(7, "sweat à capuche noir", "vetements", 45.00, 20));
-        produits_.push_back(Produit(8, "sneakers blanches", "chaussures", 89.90, 8));
-        produits_.push_back(Produit(9, "parfum royal oud", "parfumerie", 120.00, 5));
-        produits_.push_back(Produit(10, "jean slim bleu", "vetements", 39.00, 15));
-        produits_.push_back(Produit(11, "bottines en cuir", "chaussures", 110.00, 3));
-        produits_.push_back(Produit(12, "sac à dos urbain", "accessoire", 75.00, 10));
-        produits_.push_back(Produit(13, "imprimante 3D", "informatique", 199.99, 7));
-        produits_.push_back(Produit(14, "tablette graphique", "informatique", 249.99, 4));
+    produits_.push_back(ProduitElectronique(1, "ordinateur portable", "informatique", 899.99, 5));
+    produits_.push_back(ProduitElectronique(2, "souris sans fil", "accessoire", 29.99, 20));
+    produits_.push_back(ProduitElectronique(3, "clavier mecanique", "accessoire", 149.99, 8));
+    produits_.push_back(ProduitElectronique(4, "casque audio", "audio", 199.99, 12));
+    produits_.push_back(ProduitElectronique(5, "ecran 27 pouces", "informatique", 299.99, 3));
+    produits_.push_back(ProduitElectronique(6, "hub usb-c", "connectique", 49.99, 15));
+    produits_.push_back(ProduitElectronique(7, "sweat à capuche noir", "vetements", 45.00, 20));
+    produits_.push_back(ProduitElectronique(8, "sneakers blanches", "chaussures", 89.90, 8));
+    produits_.push_back(ProduitElectronique(9, "parfum royal oud", "parfumerie", 120.00, 5));
+    produits_.push_back(ProduitElectronique(10, "jean slim bleu", "vetements", 39.00, 15));
+    produits_.push_back(ProduitElectronique(11, "bottines en cuir", "chaussures", 110.00, 3));
+    produits_.push_back(ProduitElectronique(12, "sac à dos urbain", "accessoire", 75.00, 10));
+    produits_.push_back(ProduitElectronique(13, "imprimante 3D", "informatique", 199.99, 7));
+    produits_.push_back(ProduitElectronique(14, "tablette graphique", "informatique", 249.99, 4));
+}
 }
 
 // recherche produit par id
